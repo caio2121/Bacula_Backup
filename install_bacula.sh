0f1db1edf2a697ac59d1a146f156061963545a5e
Caio Abreu <72822150+caio2121@users.noreply.github.com>
2024-12-23 11:23:37 -0300

    Update install_bacula.sh

--- install_bacula.sh
+++ install_bacula.sh
@@ -1,47 +1,80 @@
 #!/bin/bash
 
-# Atualizando pacotes e instalando dependências essenciais
-apt update && apt upgrade -y
-apt install build-essential zlib1g-dev pbzip2 wget libreadline-dev -y
+# Atualizar repositórios e instalar dependências necessárias
+echo "Atualizando repositórios e instalando dependências..."
+sudo apt update && sudo apt install -y \
+    build-essential \
+    zlib1g-dev \
+    pbzip2 \
+    libpq-dev \
+    postgresql \
+    postgresql-client \
+    wget \
+    tar
 
-# Instalando o PostgreSQL e as dependências do PostgreSQL para Bacula
-apt install postgresql postgresql-contrib libpq-dev -y
+# Variáveis
+download_url="https://www.bacula.org/download/11487/?tmstv=1731045036"
+version="15.0.2"
+source_dir="/usr/src"
+bacula_tar="bacula-${version}.tar.gz"
+bacula_dir="bacula-${version}"
 
-# Baixando o código-fonte do Bacula 15.0.2
-cd /usr/src/
-wget https://ufpr.dl.sourceforge.net/project/bacula/bacula/15.0.2/bacula-15.0.2.tar.gz
-tar -zxvf bacula-15.0.2.tar.gz
-cd bacula-15.0.2
+# Baixar e renomear o arquivo do Bacula
+echo "Baixando o Bacula versão $version..."
+cd "$source_dir" || exit 1
+wget "$download_url" -O bacula-download.tmp
+mv bacula-download.tmp "$bacula_tar"
 
-# Configurando o Bacula com suporte ao PostgreSQL
-./configure --with-readline=/usr/include/readline --disable-conio --bindir=/usr/bin --sbindir=/usr/sbin \
-            --with-scriptdir=/etc/bacula/scripts --with-working-dir=/var/lib/bacula --with-logdir=/var/log \
-            --enable-smartalloc --enable-client-only=no --enable-build-dird=yes --enable-build-stored=no \
-            --with-pgsql=/usr/include/pgsql --enable-pgsql
+# Descompactar o Bacula
+echo "Descompactando o Bacula..."
+tar -zxvf "$bacula_tar"
+cd "$bacula_dir" || exit 1
 
-# Compilando e instalando o Bacula Director
+# Configurar, compilar e instalar o Bacula Director
+echo "Configurando o Bacula Director com suporte ao PostgreSQL..."
+./configure \
+    --with-postgresql \
+    --with-readline=/usr/include/readline \
+    --disable-conio \
+    --bindir=/usr/bin \
+    --sbindir=/usr/sbin \
+    --with-scriptdir=/etc/bacula/scripts \
+    --with-working-dir=/var/lib/bacula \
+    --with-logdir=/var/log \
+    --with-plugindir=/usr/lib/bacula \
+    --enable-smartalloc \
+    --enable-build-fd=no \
+    --enable-build-stored=no
+
 make -j$(nproc)
+echo "Instalando o Bacula Director..."
 make install
 make install-autostart
 
-# Desabilitando Bacula SD e Bacula FD, habilitando Bacula Director
-systemctl disable bacula-sd bacula-fd
-systemctl enable bacula-dir
+# Criar o usuário Bacula e definir permissões
+echo "Criando o usuário bacula e configurando permissões..."
+sudo useradd -r -s /bin/false bacula
+sudo chown -R bacula:bacula /var/lib/bacula
 
 # Configuração do PostgreSQL
-# Criando banco de dados e usuário no PostgreSQL
-echo "Criando usuário e banco de dados para Bacula no PostgreSQL..."
-sudo -u postgres psql -c "CREATE USER bacula WITH PASSWORD 'senha_bacula';"
-sudo -u postgres psql -c "CREATE DATABASE bacula ENCODING='UTF8' OWNER bacula;"
-sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE bacula TO bacula;"
+echo "Configurando o PostgreSQL para o Bacula Director..."
+# Dar permissões ao usuário postgres
+sudo chmod 700 /var/lib/postgresql
+sudo chown postgres:postgres /var/lib/postgresql
+
+# Executar os scripts de criação do banco de dados Bacula
+echo "Executando os scripts para configurar o banco de dados PostgreSQL..."
+sudo -u postgres bash -c "/etc/bacula/scripts/create_postgresql_database"
+sudo -u postgres bash -c "/etc/bacula/scripts/make_postgresql_tables"
+sudo -u postgres bash -c "/etc/bacula/scripts/grant_postgresql_privileges"
 
 # Configuração do Bacula Director
-echo "Por favor, edite o arquivo de configuração do Bacula Director (/etc/bacula/bacula-dir.conf) conforme necessário."
-vim /etc/bacula/bacula-dir.conf
+echo "Editando o arquivo de configuração do Bacula Director..."
+sudo vim /etc/bacula/bacula-dir.conf
 
-# Iniciando o Bacula Director
-echo "Iniciando o Bacula Director..."
-systemctl start bacula-dir
+# Iniciar e habilitar o Bacula Director
+echo "Habilitando e iniciando o Bacula Director..."
+sudo systemctl enable bacula-dir
+sudo systemctl start bacula-dir
 
-# Finalizando a instalação
-echo "Instalação concluída! O Bacula Director está configurado e em execução."
+echo "Instalação e configuração do Bacula Director concluídas."
